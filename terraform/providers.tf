c627c243eb4a9b0ac85c456735791f57b23fabf6
Uma-Karthik <parasaumakarthik.com>
2026-08-30 16:47:17 +0530

    final deploy

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "uma-terraform-state-8997"
+    key    = "aws-3-tier/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
